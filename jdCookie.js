c6018373702e914667534f8a386ff2d44d534f4c
wangxiaolin <wangxiaolin@victorysoft.com.cn>
2021-01-25 15:33:02 +0800

    up

--- jdCookie.js
+++ jdCookie.js
@@ -4,9 +4,8 @@
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
-  'pt_pin=;pt_key=;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_pin=;pt_key=;',
-  'pt_pin=;pt_key=-;'//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
+  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
 if (process.env.JD_COOKIE) {
